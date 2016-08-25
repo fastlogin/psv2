0054235fa388f947087bf6edbd8daab8eca603f7
fastlogin <gd264@cornell.edu>
2016-08-25 02:37:46 -0400

    uhuh up to acitivities

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,5 +13,61 @@ function content_blink(content) {
 $(document).ready(function(){
 	var doc = $(document);
 	var header_cursor = doc.find(".js-blink");
+	var about_me = doc.find(".js-about-me");
+	var java_code_box = doc.find(".js-java");
+	var java_code_button = doc.find(".js-java-button");
+	var ruby_code_box = doc.find(".js-ruby");
+	var ruby_code_button = doc.find(".js-ruby-button");
+	var python_code_box = doc.find(".js-python");
+	var python_code_button = doc.find(".js-python-button");
+	var reading_button = doc.find(".js-reading-button");
+	var gaming_button = doc.find(".js-gaming-button");
+	var reading_text = doc.find(".js-reading-text");
+	var gaming_text = doc.find(".js-gaming-text");
+	var current_language;
+
+	hljs.initHighlightingOnLoad();
+
+	java_code_box.toggleClass("css-languages-active");
+	current_language = java_code_box;
 	setInterval(function(){content_blink(header_cursor);}, 420);
+
+	java_code_button.click(function(){
+		if (current_language == java_code_box) {
+			return;
+		}
+		current_language.toggleClass("css-languages-active");
+		java_code_box.toggleClass("css-languages-active");
+		current_language = java_code_box;
+		about_me.html("AboutMe.java");
+	})
+
+	ruby_code_button.click(function(){
+		if (current_language == ruby_code_box) {
+			return;
+		}
+		current_language.toggleClass("css-languages-active");
+		ruby_code_box.toggleClass("css-languages-active");
+		current_language = ruby_code_box;
+		about_me.html("AboutMe.rb");
+	})
+
+	python_code_button.click(function(){
+		if (current_language == python_code_box) {
+			return;
+		}
+		current_language.toggleClass("css-languages-active");
+		python_code_box.toggleClass("css-languages-active");
+		current_language = python_code_box;
+		about_me.html("AboutMe.py");
+	})
+
+	reading_button.click(function(){
+		reading_text.toggle("slide");
+	})
+
+	gaming_button.click(function(){
+		gaming_text.toggle("slide");
+	})
+
 })
